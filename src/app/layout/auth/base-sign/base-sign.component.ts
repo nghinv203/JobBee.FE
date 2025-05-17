0b23724b9343d15a6ef6ea2bd4632200ea6a36ac
NghiNV <nghinv03@gmail.com>
2025-05-18 00:41:59 +0700

    feat: sign up UI

--- src/app/layout/auth/base-sign/base-sign.component.ts
+++ src/app/layout/auth/base-sign/base-sign.component.ts
@@ -1,8 +1,14 @@
 import { Component } from '@angular/core';
+import {TranslatePipe} from '@ngx-translate/core';
+import {RouterLink} from '@angular/router';
 
 @Component({
   selector: 'app-base-sign',
-  imports: [],
+  imports: [
+    TranslatePipe,
+    RouterLink
+  ],
+  standalone: true,
   templateUrl: './base-sign.component.html',
   styleUrl: './base-sign.component.scss'
 })
--- src/app/layout/auth/sign-up/sign-up.component.ts
+++ src/app/layout/auth/sign-up/sign-up.component.ts
@@ -1,8 +1,23 @@
 import { Component } from '@angular/core';
+import {BaseSignComponent} from '../base-sign/base-sign.component';
+import {TranslatePipe} from '@ngx-translate/core';
+import {NzOptionComponent, NzSelectComponent} from 'ng-zorro-antd/select';
+import {NzCheckboxComponent} from 'ng-zorro-antd/checkbox';
+import {NzInputDirective} from 'ng-zorro-antd/input';
+import {NzButtonComponent} from 'ng-zorro-antd/button';
 
 @Component({
   selector: 'app-sign-up',
-  imports: [],
+  imports: [
+    BaseSignComponent,
+    TranslatePipe,
+    NzSelectComponent,
+    NzOptionComponent,
+    NzCheckboxComponent,
+    NzInputDirective,
+    NzButtonComponent
+  ],
+  standalone: true,
   templateUrl: './sign-up.component.html',
   styleUrl: './sign-up.component.scss'
 })
