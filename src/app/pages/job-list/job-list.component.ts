107b8766afe8ff4822512da145a16c3553c30a58
NghiNV <nghinv03@gmail.com>
2025-06-22 20:48:03 +0700

    fix: overbuget

--- src/app/pages/job-list/job-list.component.ts
+++ src/app/pages/job-list/job-list.component.ts
@@ -1,7 +1,9 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {TranslatePipe} from '@ngx-translate/core';
 import {BreadcrumbComponent} from '../../shared/reuseComponents/breadcrumb/breadcrumb.component';
 import {JobSearchComponent} from './job-search/job-search.component';
+import {JobListService} from "./job-list.service";
+import {IJob} from "../entities/job/job.model";
 
 @Component({
   selector: 'app-job-list',
@@ -14,9 +16,20 @@ import {JobSearchComponent} from './job-search/job-search.component';
   templateUrl: './job-list.component.html',
   styleUrl: './job-list.component.scss'
 })
-export class JobListComponent {
-  constructor() {
+export class JobListComponent implements OnInit{
+
+  jobs: IJob[] = [];
+
+  constructor(private jobListService: JobListService) {
   }
 
+  ngOnInit(): void {
+    this.jobListService.getJobs('').subscribe(res => {
+      this.jobs = res.data ?? [];
+    });
+  }
 
+  handleSearch(event: any) {
+    console.log(event);
+  }
 }
