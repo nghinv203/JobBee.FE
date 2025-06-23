9ad58b206dcf7d9e47700486da72c3096e4f2ecb
NghiNV <nghinv03@gmail.com>
2025-06-24 05:49:43 +0700

    feat: paginator interceptor

--- src/app/core/interceptors/paginator/paginator.interceptor.ts
+++ src/app/core/interceptors/paginator/paginator.interceptor.ts
@@ -1,5 +1,36 @@
-import { HttpInterceptorFn } from '@angular/common/http';
+import { HttpInterceptorFn, HttpParams } from '@angular/common/http';
+
+const PAGINATION_URLS = ['/api/jobs/search'];
+
+interface RequestBody {
+  params?: Record<string, any>;
+  page?: number;
+  pageSize?: number;
+}
 
 export const paginatorInterceptor: HttpInterceptorFn = (req, next) => {
+  if (PAGINATION_URLS.some(url => req.url.includes(url))) {
+    if (req.method === 'GET') {
+      let modifiedParams = req.params;
+      if (!modifiedParams.has('page')) {
+        modifiedParams = modifiedParams.set('page', '1');
+      }
+      if (!modifiedParams.has('pageSize')) {
+        modifiedParams = modifiedParams.set('pageSize', '20');
+      }
+      return next(req.clone({ params: modifiedParams }));
+    } else if (req.method === 'POST') {
+      const body: RequestBody = req.body || {};
+      const params = body.params || {};
+
+      const modifiedBody = {
+        ...params,
+        page: body.page ?? 1,
+        pageSize: body.pageSize ?? 20
+      };
+
+      return next(req.clone({ body: modifiedBody }));
+    }
+  }
   return next(req);
 };
