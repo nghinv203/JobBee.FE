2bf70cf85ac921c0217c83d7d8bda51780b67309
NghiNV <nghinv03@gmail.com>
2025-06-25 22:59:13 +0700

    feat: log for monitor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 import {TranslateModule, TranslateService} from "@ngx-translate/core";
 import {RouterOutlet} from '@angular/router';
+import { Analytics } from "@vercel/analytics/next"
 
 @Component({
   selector: 'app-root',
