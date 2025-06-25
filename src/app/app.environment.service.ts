ec3cfd738b8d75476615f7bebf1e142dfc211879
NghiNV <nghinv03@gmail.com>
2025-06-25 18:45:52 +0700

    fix: mix content

--- src/app/app.environment.service.ts
+++ src/app/app.environment.service.ts
@@ -2,6 +2,6 @@ import {Injectable} from '@angular/core';
 
 @Injectable({ providedIn: 'root' })
 export class AppEnvironmentService {
-  HOST_OS = 'http://54.175.186.165:5000/api';
+  HOST_OS = 'https://54.175.186.165:5001/api';
   HOST_PRODUCTION = ''; // use when deploying
 }
