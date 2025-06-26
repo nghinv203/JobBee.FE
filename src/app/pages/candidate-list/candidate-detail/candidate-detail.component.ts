ae4a90387184c8909280f21e0226004ea2e9effe
tubahe171912 <buianhtu293@gmail.com>
2025-06-27 00:03:12 +0700

    feat: candidate list

--- src/app/pages/candidate-list/candidate-detail/candidate-detail.component.ts
+++ src/app/pages/candidate-list/candidate-detail/candidate-detail.component.ts
@@ -1,11 +1,17 @@
-import { Component } from '@angular/core';
+import {Component, EventEmitter, Output} from '@angular/core';
 
 @Component({
   selector: 'app-candidate-detail',
+  standalone: true,
   imports: [],
   templateUrl: './candidate-detail.component.html',
   styleUrl: './candidate-detail.component.scss'
 })
 export class CandidateDetailComponent {
+  @Output() isProfile = new EventEmitter<boolean>();
 
+
+  emitMessage() {
+    this.isProfile.emit(false);
+  }
 }
--- src/app/pages/candidate-list/candidate-list.component.ts
+++ src/app/pages/candidate-list/candidate-list.component.ts
@@ -1,12 +1,27 @@
 import { Component } from '@angular/core';
+import {HeaderComponent} from '../../layout/main/header/header.component';
+import {CandidateSearchComponent} from './candidate-search/candidate-search.component';
+import {CandidateDetailComponent} from './candidate-detail/candidate-detail.component';
 
 @Component({
   selector: 'app-candidate-list',
   standalone: true,
-  imports: [],
+  imports: [
+    HeaderComponent,
+    CandidateSearchComponent,
+    CandidateDetailComponent
+  ],
   templateUrl: './candidate-list.component.html',
   styleUrl: './candidate-list.component.scss'
 })
 export class CandidateListComponent {
+  isProfile: boolean = false;
 
+  OpenProfile() {
+    this.isProfile = true;
+  }
+
+  isOpenProfile(isOpen: boolean) {
+    this.isProfile = isOpen;
+  }
 }
