3fb51d2ef68dbb70ed9a881836ab0d50237f9edf
NghiNV <nghinv03@gmail.com>
2025-06-30 20:17:33 +0700

    fix: update host os

--- src/app/app.environment.service.ts
+++ src/app/app.environment.service.ts
@@ -2,6 +2,6 @@ import {Injectable} from '@angular/core';
 
 @Injectable({ providedIn: 'root' })
 export class AppEnvironmentService {
-  HOST_OS = 'https://54.175.186.165:5001/api';
+  HOST_OS = 'https://jobbee.work.gd:5001/api';
   HOST_PRODUCTION = ''; // use when deploying
 }
