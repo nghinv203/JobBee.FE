9b22589b32401e9dcc76fb1c63a1c3097cdb0013
NghiNV <nghinv03@gmail.com>
2025-07-01 00:57:31 +0700

    fix: job list, job detail

--- src/app/pages/employer-list/employer-detail/employer-detail.component.ts
+++ src/app/pages/employer-list/employer-detail/employer-detail.component.ts
@@ -1,19 +1,24 @@
-import {Component, EventEmitter, Output} from '@angular/core';
-import {TranslatePipe} from '@ngx-translate/core';
+import {Component, EventEmitter, OnInit, Output} from '@angular/core';
+import {ActivatedRoute} from '@angular/router';
 
 @Component({
   selector: 'app-employer-detail',
   standalone: true,
-  imports: [
-    TranslatePipe
-  ],
+  imports: [],
   templateUrl: './employer-detail.component.html',
   styleUrl: './employer-detail.component.scss'
 })
-export class EmployerDetailComponent {
+export class EmployerDetailComponent implements OnInit{
   @Output() sendMessage = new EventEmitter<boolean>();
+
+  constructor(private route: ActivatedRoute) {
+  }
 
   handleEmit() {
     this.sendMessage.emit(false);
+  }
+
+  ngOnInit(): void {
+    const id = this.route.snapshot.paramMap.get('id')!;
   }
 }
